bd97f439f6609c5858a9a083a2c6f6ed2a633cdf
evanfellman <evan.fellman@gmail.com>
2019-11-19 18:35:43 -0500

    Stops at walls when hit from left

--- src/Game/Player.java
+++ src/Game/Player.java
@@ -16,30 +16,39 @@ public class Player extends Thing {
 	@Override
 	void move(ArrayList<ArrayList<Thing>> map) {
 		Main.removeFromMap(this);
-		this.x += this.dx;
 		this.dy += Main.GRAVITY;
 		if(this.dy > 5) {
 			this.dy = 5;
 		}
 		if(Main.isAPressed) {
 			this.dx = -1;
-//			for(int i = (int) (this.dx - Main.SPRITE_WIDTH); i >= 0; i++) {
-//				for(int j = 0; j <= Main.SPRITE_HEIGHT; j++) {
-//					Thing a = Main.getFromMap(this.x + i, this.y + j);
-//					if(a != null && a.id.equals("wall")) {
-//						this.x = a.x + Main.SPRITE_WIDTH;
-//						this.dx = 0;
-//					}
-//				}
-//			}
+			for(int i = (int) (this.dx - Main.SPRITE_WIDTH); i >= 0; i++) {
+				for(int j = -1 * (Main.SPRITE_HEIGHT + 1); j <= Main.SPRITE_HEIGHT; j++) {
+					Thing a = Main.getFromMap(this.x + i, this.y + j);
+					if(a != null && a.id.equals("wall")) {
+						this.x = a.x + (2 * Main.SPRITE_WIDTH);
+						this.dx = 0;
+					}
+				}
+			}
 		} else if(Main.isDPressed) {
 			this.dx = 1;
+			for(int i = 0; i <= this.dx + Main.SPRITE_WIDTH; i++) {
+				for(int j = 0; j <= Main.SPRITE_HEIGHT; j++) {
+					Thing a = Main.getFromMap(this.x + i, this.y - j);
+					if(a != null && a.id.equals("wall")) {
+						this.x = -1 + (a.x - Main.SPRITE_WIDTH);
+						this.dx = 0;
+					}
+				}
+			}
 		} else {
 			this.dx = 0;
 		}
+		this.x += this.dx;
 		if(dy >= 0) {
 			for(int i = 0; i <= (this.dy + Main.SPRITE_HEIGHT); i++) {
-				for(int j = -1 * (int) (0.5 * Main.SPRITE_WIDTH); j < Main.SPRITE_WIDTH; j++) {
+				for(int j = -1 * (int) ( Main.SPRITE_WIDTH); j < Main.SPRITE_WIDTH; j++) {
 					Thing a = Main.getFromMap(this.x - j, this.y + i);
 					if(a != null && a.id.equals("wall")) {
 						this.y = a.y - Main.SPRITE_HEIGHT;
